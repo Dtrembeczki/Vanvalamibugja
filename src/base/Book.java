12520a64bea360915bb5970803345af3713a8905
David Trembeczki <tremb.d98@gmail.com>
2022-04-09 22:52:22 +0200

    Könyv gyakorlás 0409

--- src/base/Book.java
+++ src/base/Book.java
@@ -1,47 +1,88 @@
 package base;
 
 public class Book {
-	
-	//Datas
+
+	// Datas
 	private String title;
 	private String author;
 	private int year;
 	private int price;
-	
+	private int pages;
+
 	java.time.LocalDate currentDate = java.time.LocalDate.now();
 	int cur_year = currentDate.getYear();
-	
-	public Book(String title, String author, int year, int price) {
+
+	public Book(String title, String author, int price, int pages) {
 		super();
 		this.title = title;
 		this.author = author;
-		this.year = year;
+		this.year = cur_year;
 		this.price = price;
+		this.pages = pages;
 	}
-	
+
 	public Book(String title, String author) {
 		super();
 		this.title = title;
 		this.author = author;
 		this.price = 2500;
 		this.year = cur_year;
+		this.pages = 100;
+	}
+
+	// myFunctions
+
+	public void raisePrice(int percent) {
+		this.price += this.price * (percent / 100);
+	}
+
+	// Írjon osztályszintû metódust, amely két paraméterként kapott könyv közül a
+	// hosszabbat adja vissza (getLonger()). Azonos oldalszám esetén az elsõ
+	// könyvet.
+	public boolean getLonger(Book b1, Book b2) {
+		if (b1.pages >= b2.pages) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// Írjon példányszintû metódust, amely igazat ad vissza, ha a könyv oldalszáma
+	// páros (hasEvenPages()).
+	public boolean hasEvenPages() {
+		if (this.pages % 2 == 0) {
+			return true;
+		}
+
+		return false;
 	}
 	
-	
-	//myFunctions
-	
-	public void raisePrice(int percent) {
-		this.price += this.price *(percent/100);
+	//Tomb kiiro
+	public static void listBookArray(Book[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+
+	//Leghosszabb könyv
+	public static Book getLongestBook(Book[] array) {
+		Book longestBook = array[0];
+		
+		for (int i = 0; i < array.length; i++) {
+			
+			if(array[i].pages > longestBook.pages) {
+				array[i] = longestBook;
+			}
+		}
+		return longestBook;
 	}
 	
 	
 	
 	@Override
 	public String toString() {
-		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", price=" + price + "]";
+		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", price=" + price + "Pages:" + pages + "]";
 	}
-
-
 
 	// Getters&Setters
 	public String getTitle() {
@@ -75,7 +116,18 @@ public class Book {
 	public void setPrice(int price) {
 		this.price = price;
 	}
-	
-	
-	
+
+	public int getPages() {
+		return pages;
+	}
+
+	public void setPages(int pages) {
+
+		if (pages < 0) {
+			this.pages = 0;
+		} else {
+			this.pages = pages;
+		}
+	}
+
 }
--- src/running/BookTest.java
+++ src/running/BookTest.java
@@ -1,15 +1,45 @@
 package running;
 
 import base.Book;
+import java.util.*;
 
 public class BookTest {
 
 	public static void main(String[] args) {
-		Book b1 = new Book("Piszkos Fred a kapitány", "Rejtõ Jenõ", 1963, 2500);
-		Book b2 = new Book("Harry Potter", "J.K. Rowling");
 		
-		System.out.println(b1);
-		System.out.println(b2);
+		Book[] bookArray = new Book[3];
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		String title;
+		String author;
+		int price;
+		int pages;
+		
+		for (int i = 0; i < bookArray.length; i++) {
+			
+			/*System.out.println("Title: ");
+			bookArray[i].setTitle(input.next());
+			System.out.println("Author: ");
+			bookArray[i].setAuthor(input.next());
+			System.out.println("Price: ");
+			bookArray[i].setPrice(input.nextInt());
+			System.out.println("Pages: ");
+			bookArray[i].setPages(input.nextInt());*/
+			
+			System.out.print("Title: ");
+			title = input.nextLine();
+			System.out.print("Author: ");
+			author = input.nextLine();
+			System.out.print("price: ");
+			price = input.nextInt();
+			System.out.print("pages: ");
+			pages = input.nextInt();
+			
+			bookArray[i] = new Book(title, author, price, pages);
+		}
+		
+		Book.listBookArray(bookArray);
+		System.out.println("Longest book: " + Book.getLongestBook(bookArray).getTitle());
 	}
-
+	
 }
